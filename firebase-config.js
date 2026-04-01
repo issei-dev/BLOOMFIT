d9c06044524a008b1a6dcdfaf2d863b8e9a0a5c0
issei-dev <isseitogasgi.biz@gmail.com>
2026-04-01 11:47:40 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Firebase SDK (CDN版を使用)
+// ※ index.html の <head> 内で先に CDN を読み込むこと
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
